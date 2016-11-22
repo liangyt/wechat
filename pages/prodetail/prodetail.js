70f8d88de84457134fe34edc40241534c7f3ce07
liangyt <liangyongtong@foxmail.com>
2016-11-22 22:12:33 +0800

    add weui wxss

---
+++ pages/prodetail/prodetail.js
@@ -0,0 +1,27 @@
+var sliderWidth = 96; // 需要设置slider的宽度，用于计算中间位置
+
+Page({
+    data: {
+        list: [1, 2, 3],
+        tabs: ["商品详情", "商品规格", "售后服务"],
+        activeIndex: "0",
+        sliderOffset: 0,
+        sliderLeft: 0
+    },
+    onLoad: function () {
+        var that = this;
+        wx.getSystemInfo({
+            success: function(res) {
+                that.setData({
+                    sliderLeft: (res.windowWidth / that.data.tabs.length - sliderWidth) / 2
+                });
+            }
+        });
+    },
+    tabClick: function (e) {
+        this.setData({
+            sliderOffset: e.currentTarget.offsetLeft,
+            activeIndex: e.currentTarget.id
+        });
+    }
+});
