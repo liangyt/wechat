1d855327443f1a9a8fbf3df5c3f5ede180133dbb
liangyt <liangyongtong@foxmail.com>
2016-11-27 16:44:46 +0800

    update

---
+++ pages/center/center.js
@@ -0,0 +1,30 @@
+var app = getApp()
+
+Page({
+  data:{
+    userInfo: {}
+  },
+  onLoad:function(options){
+    var that = this;
+    // 页面初始化 options为页面跳转所带来的参数
+    // 获取个人信息
+    app.getUserInfo(function(userInfo){
+      //更新数据
+      that.setData({
+        userInfo:userInfo
+      })
+    })
+  },
+  onReady:function(){
+    // 页面渲染完成
+  },
+  onShow:function(){
+    // 页面显示
+  },
+  onHide:function(){
+    // 页面隐藏
+  },
+  onUnload:function(){
+    // 页面关闭
+  }
+})
