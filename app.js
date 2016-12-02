2ebcda740f420f50440ed74209e3e9e0340e352b
liangyongtong <liangyongtong@foxmail.com>
2016-12-02 21:42:43 +0800

    udate

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 //app.js
 App({
+  // 系统启动后调用
   onLaunch: function () {
-    //调用API从本地缓存中获取数据
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
+    
   },
   getUserInfo:function(cb){
     var that = this
---
+++ pages/myorder/myorder.js
@@ -0,0 +1,40 @@
+// pages/myorder/myorder.js
+Page({
+  data:{
+    orders:[
+      {
+        list:[
+          {name:"iPhone 6s iPhone 6s ",price:6488,num:1},
+          {name:"iPhone 6s iPhone 6s ",price:6488,num:1}
+          ]
+      },
+      {
+        list:[
+          {name:"iPhone 6s iPhone 6s ",price:6488,num:1}
+          ]
+      },
+      {
+        list:[
+          {name:"iPhone 6s iPhone 6s ",price:6488,num:1},
+          {name:"iPhone 6s iPhone 6s ",price:6488,num:1},
+          {name:"iPhone 6s iPhone 6s ",price:6488,num:1}
+          ]
+      }
+    ]
+  },
+  onLoad:function(options){
+    // 页面初始化 options为页面跳转所带来的参数
+  },
+  onReady:function(){
+    // 页面渲染完成
+  },
+  onShow:function(){
+    // 页面显示
+  },
+  onHide:function(){
+    // 页面隐藏
+  },
+  onUnload:function(){
+    // 页面关闭
+  }
+})
