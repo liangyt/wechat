37af05be336e52003423de9609b87139db633606
liangyongtong <liangyongtong@foxmail.com>
2016-12-03 18:51:45 +0800

    update

---
+++ pages/address/address.js
@@ -0,0 +1,24 @@
+Page({
+  data:{
+    addresses:[
+          {address:"北京市长安街天安门001号",name:'同同',mobile:13888888888},
+          {address:"北京市长安街天安门001号",name:'同同',mobile:13888888888},
+          {address:"北京市长安街天安门001号",name:'同同',mobile:13888888888},
+    ]
+  },
+  onLoad:function(options){
+    // 页面初始化 options为页面跳转所带来的参数
+  },
+  onReady:function(){
+    // 页面渲染完成
+  },
+  onShow:function(){
+    // 页面显示
+  },
+  onHide:function(){
+    // 页面隐藏
+  },
+  onUnload:function(){
+    // 页面关闭
+  }
+})
